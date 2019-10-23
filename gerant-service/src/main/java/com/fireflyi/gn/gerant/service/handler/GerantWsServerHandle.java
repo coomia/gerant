c705b93d8a7e9fccf65d41caa31c27b0e386b472
lishihao4 <lishihao@jd.com>
2019-10-23 09:33:08 +0800

    :sparkles:ws握手协议

--- gerant-service/src/main/java/com/fireflyi/gn/gerant/service/handler/GerantWsServerHandle.java
+++ gerant-service/src/main/java/com/fireflyi/gn/gerant/service/handler/GerantWsServerHandle.java
@@ -1,8 +1,15 @@
 package com.fireflyi.gn.gerant.service.handler;
 
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
+import io.netty.util.CharsetUtil;
 
 /**
  * @author by lishihao
@@ -11,6 +18,10 @@ import io.netty.channel.SimpleChannelInboundHandler;
  */
 @ChannelHandler.Sharable
 public class GerantWsServerHandle extends SimpleChannelInboundHandler<Object> {
+
+    private WebSocketServerHandshaker handshaker;
+
+    private static final String WEB_SOCKET_URL = "ws://127.0.0.1:8666/ws";
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
@@ -34,6 +45,48 @@ public class GerantWsServerHandle extends SimpleChannelInboundHandler<Object> {
         System.out.println("异常链接断开");
         cause.printStackTrace() ;
         ctx.close() ;
+    }
+
+    /**
+     * 处理客户端向服务端发起http握手请求的业务
+     * @param ctx
+     * @param req
+     */
+    private void handHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        if (!req.getDecoderResult().isSuccess() || !("websocket".equals(req.headers().get("Upgrade")))) {
+            //处理不是握手请求
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+
+        WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(WEB_SOCKET_URL, null, false);
+        handshaker = wsFactory.newHandshaker(req);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
+        } else {
+            handshaker.handshake(ctx.channel(), req);
+        }
+
+    }
+
+    /**
+     * 服务端向客户端响应消息
+     *
+     * @param ctx
+     * @param req
+     * @param res
+     */
+    private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse res) {
+        if (res.getStatus().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+        }
+        //服务端向客户端发送数据
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (res.getStatus().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);//关闭连接
+        }
     }
 
 }
